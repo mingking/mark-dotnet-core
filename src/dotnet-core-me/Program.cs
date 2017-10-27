114403e90e0a2e62847d4bc91607853538cb52f5
mingking <mingking0947@gmail.com>
2017-10-27 11:15:38 +0800

    change host name

--- src/dotnet-core-me/Program.cs
+++ src/dotnet-core-me/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace dotnet_core_me
 {
@@ -11,19 +12,18 @@ namespace dotnet_core_me
     {
         public static void Main(string[] args)
         {
+/*
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+*/
+
             /*
-                        var host = new WebHostBuilder()
-                            .UseKestrel()
-                            .UseContentRoot(Directory.GetCurrentDirectory())
-                            .UseIISIntegration()
-                            .UseStartup<Startup>()
-                            .Build();
-
-            */
-
             //refer to :http://kerryritter.com/deploying-asp-net-core-to-heroku/
             var url = $"http://*:{Environment.GetEnvironmentVariable("PORT")}/";
-
             Console.WriteLine($"Using Url: {url}");
 
             var host = new WebHostBuilder()
@@ -32,6 +32,17 @@ namespace dotnet_core_me
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseUrls(url)
+                .Build();
+            */
+
+            //refer to :http://codersblock.com/blog/how-to-run-net-on-heroku/
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
                 .Build();
 
             host.Run();
